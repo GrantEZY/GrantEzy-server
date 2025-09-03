9229ae2afae1f8f705cf8079aab1fc55f18da4f2
inthrakumar <inthrak04@gmail.com>
2025-09-03 14:46:23 +0530

    feat:  basic setup for eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,134 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginJest from "eslint-plugin-jest";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default tseslint.config(
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: [
+      "**/*.spec.{js,ts,jsx,tsx}",
+      "**/*.test.{js,ts,jsx,tsx}",
+      "**/jest.config.{js,ts}",
+      "**/eslint.config.{js,ts}",
+      "**/vite.config.{js,ts}",
+      "**/webpack.config.{js,ts}",
+      "**/rollup.config.{js,ts}",
+      "**/tailwind.config.{js,ts}",
+      "**/postcss.config.{js,ts}",
+      "**/dist/**",
+      "**/coverage/**",
+      "**/logs/**",
+      "**/node_modules/**",
+    ],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:jest/recommended",
+  ),
+  eslintConfigPrettier,
   {
+    plugins: { jest: pluginJest },
+
     languageOptions: {
       globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        ...pluginJest.environments.globals.globals,
       },
     },
-  },
-  {
+
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      // C
+      camelcase: "off",
+      "capitalized-comments": "off",
+      "default-param-last": "error",
+
+      // E
+      eqeqeq: "error",
+      "max-params": ["warn", 3],
+
+      // N
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+      "no-empty-function": "warn",
+      "no-param-reassign": "error",
+
+      // P
+      "padding-line-between-statements": [
+        "off",
+        {
+          blankLine: "always",
+          prev: "*",
+          next: "return",
+        },
+        {
+          blankLine: "always",
+          prev: "*",
+          next: "function",
+        },
+        {
+          blankLine: "always",
+          prev: "*",
+          next: "block-like",
+        },
+        {
+          blankLine: "always",
+          prev: ["import"],
+          next: "*",
+        },
+        {
+          blankLine: "any",
+          prev: ["import"],
+          next: ["import"],
+        },
+        {
+          blankLine: "always",
+          prev: ["const", "let", "var"],
+          next: "*",
+        },
+        {
+          blankLine: "any",
+          prev: ["const", "let", "var"],
+          next: ["const", "let", "var"],
+        },
+      ],
+      "prefer-const": "error",
+      "prefer-destructuring": "error",
+      "prefer-object-spread": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrors: "none",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      "sort-keys": [
+        "off",
+        "asc",
+        {
+          natural: true,
+          minKeys: 5,
+        },
+      ],
+      "sort-vars": "error",
+      "sort-imports": ["off"],
     },
   },
-);
+];
