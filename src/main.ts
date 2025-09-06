9672d10b5089747e68c1f2db14e4d7a92bd17290
inthrakumar <inthrak04@gmail.com>
2025-09-06 10:36:38 +0530

    feat(main): add cors helmet and cookie-parser setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
+/**
+ * Main entry point of the application.
+ * Sets up and starts the NestJS server with necessary middleware and configurations.
+ * @file src/main.ts
+ */
+
 import {NestFactory} from "@nestjs/core";
 
 import {AppModule} from "./app.module";
 
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
 async function initServer() {
     const app = await NestFactory.create(AppModule);
+    app.enableShutdownHooks();
+    app.use(helmet());
+    app.use(cookieParser());
+
+    app.setGlobalPrefix("api/v1"); // Set v1 API prefix for all the routes
+    app.enableCors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        allowedHeaders:
+            "Content-Type, Accept, Authorization, X-Requested-With ,Event-Id, X-Csrf-Token",
+    });
+
     await app.listen(process.env.PORT ?? 3000);
 }
 initServer();
