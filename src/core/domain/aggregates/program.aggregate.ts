8e6ea68635d0f448bb59f3b364ad6089b5d66e98
inthrakumar <inthrak04@gmail.com>
2025-09-14 10:05:52 +0530

    feat(aggregate): add program manager relation

--- src/core/domain/aggregates/program.aggregate.ts
+++ src/core/domain/aggregates/program.aggregate.ts
@@ -9,6 +9,7 @@ import {
     BeforeInsert,
     BeforeUpdate,
     ManyToOne,
+    OneToOne,
 } from "typeorm";
 import {Organization} from "../entities/organization.entity";
 import {ProgramDetails} from "../value-objects/program.details.object";
@@ -18,6 +19,7 @@ import {Money} from "../value-objects/project.metrics.object";
 import {TRL} from "../constants/trl.constants";
 import {Cycle} from "./cycle.aggregate";
 import {slugify} from "../../../shared/helpers/slug.generator";
+import {User} from "./user.aggregate";
 @Entity({name: "programs"})
 export class Program {
     @PrimaryGeneratedColumn("uuid")
@@ -90,6 +92,17 @@ export class Program {
 
     @Column({unique: true, nullable: true})
     slug: string;
+
+    @Column()
+    managerId: string;
+
+    @OneToOne(() => User, {
+        onDelete: "SET NULL",
+        cascade: false,
+        eager: false,
+    })
+    @JoinColumn({name: "managerId"})
+    manager: User;
 
     @CreateDateColumn()
     createdAt: Date;
