61dba7b271c3cf2e17afb00ea4054eaf831de59b
inthrakumar <inthrak04@gmail.com>
2025-10-11 15:11:55 +0530

    feat(email): add nodemailer for email for development

--- src/ports/outputs/email/email.service.port.ts
+++ src/ports/outputs/email/email.service.port.ts
@@ -1,5 +1,8 @@
+import {Transporter} from "nodemailer";
+
 export interface EmailServicePort {
     sendEmail(to: string, subject: string, body: string): Promise<boolean>;
+    createTransport(): Transporter;
 }
 
 export const EMAIL_SERVICE_PORT = Symbol("EmailServicePort");
