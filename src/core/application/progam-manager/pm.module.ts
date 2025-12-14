895f7e45e20df13c8ee596c858ca10027340dc6a
inthrakumar <inthrak04@gmail.com>
2025-12-14 19:37:59 +0530

    feat: add controller , services for modifying cycle status

--- src/core/application/progam-manager/pm.module.ts
+++ src/core/application/progam-manager/pm.module.ts
@@ -1,8 +1,9 @@
 import {Module} from "@nestjs/common";
 import {ProgramManagerService} from "../../domain/services/program-manager/pm.service";
+import {ProgramManagerConfigManagementService} from "../../domain/services/program-manager/pm.cfg.management.service";
 import {ProgramManagerController} from "../../../infrastructure/driving/http/api/v1/pm.controller";
 @Module({
     controllers: [ProgramManagerController],
-    providers: [ProgramManagerService],
+    providers: [ProgramManagerService, ProgramManagerConfigManagementService],
 })
 export class ProgramManagerModule {}
